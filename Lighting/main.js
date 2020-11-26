00e618d6666977a14e0d39e94dcabb4def03b81b
hrswcksono <57925757+hrswcksono@users.noreply.github.com>
2020-11-26 07:08:33 +0700

    Update main.js

--- Lighting/main.js
+++ Lighting/main.js
@@ -3,8 +3,8 @@ var cam = new THREE.PerspectiveCamera(45, window.innerWidth/window.innerHeight,1
 var renderer = new THREE.WebGL1Renderer({ antialias: true });
 scene.background = new THREE.Color('0x0a0a0a');
 renderer.setSize(window.innerWidth, window.innerHeight);
-renderer.shadowMap.enabled = true;
-renderer.shadowMap.type = THREE.BasicShadowMap;
+renderer.shadowMap.enabled = true; // mengaktifkan bayangan
+renderer.shadowMap.type = THREE.BasicShadowMap; // menggunakan bayangan versi basic
 cam.position.z += 15;
 document.body.appendChild(renderer.domElement);
 
@@ -14,8 +14,8 @@ var boxMat2 = new THREE.MeshPhongMaterial({
 });
 
 var cube2 = new THREE.Mesh(box, boxMat2);
-cube2.receiveShadow = true;
-cube2.castShadow = true;
+cube2.receiveShadow = true; // agar benda bisa mendapatkan bayangan
+cube2.castShadow = true; // agar benda bisa membentuk bayangan
 scene.add(cube2);
 
 var plane = new THREE.PlaneGeometry(1000,1000,500,500);
@@ -28,39 +28,38 @@ planeMesh.position.set(0, -1, 0);
 planeMesh.rotation.x = -Math.PI/2;
 scene.add(planeMesh);
 
-window.addEventListener('resize', function() {
+window.addEventListener('resize', function() { // auto size window
     renderer.setSize(this.window.innerWidth, this.window.innerHeight);
     cam.aspect = this.window.innerWidth/this.window.innerHeight;
     cam.updateProjectionMatrix();
 })
 
-// var rectLight = new THREE.RectAreaLight(0xffffff, 1, 10, 10);
+// var rectLight = new THREE.RectAreaLight(0xffffff, 1, 10, 10); //RectAreaLight
 // rectLight.position.set(5, 5, 0);
 // rectLight.lookAt(1, 1, 1);
 // scene.add(rectLight);
-
 // var rectLightHelper = new THREE.RectAreaLightHelper( rectLight );
 // rectLight.add( rectLightHelper );
 
-var ambient = new THREE.AmbientLight(0x404040);
+var ambient = new THREE.AmbientLight(0x404040); // Ambient Light
 scene.add(ambient);
 
-var pointLight = new THREE.PointLight(0xff0000,0.5,50);
+var pointLight = new THREE.PointLight(0xff0000,0.5,50); //Point Light
 pointLight.position.set(2,2,2);
 scene.add(pointLight);
 scene.add(new THREE.PointLightHelper(pointLight,0.2,0x00ff00));
 
-// var hemi = new THREE.HemisphereLight(0x0000ff, 0x000000, 0,5);
+// var hemi = new THREE.HemisphereLight(0x0000ff, 0x000000, 0,5); // HemiSphere Light
 // scene.add(hemi);
 
-// var directionalLight = new THREE.DirectionalLight(0x00ff00, 0.5);
+// var directionalLight = new THREE.DirectionalLight(0x00ff00, 0.5); // Directional Light
 // directionalLight.position.set(2,2,0);
 // directionalLight.target.position.set(3,2,0);
 // directionalLight.target.updateMatrixWorld();
 // scene.add(directionalLight);
 // scene.add(new THREE.DirectionalLightHelper(directionalLight));
 
-var spotlight = new THREE.SpotLight(0x0000ff, 0.5, 5, Math.PI/10,);
+var spotlight = new THREE.SpotLight(0x0000ff, 0.5, 5, Math.PI/10,); // SpotLight
 spotlight.position.set(2,2,0);
 // spotlight.target.position.set(2,3,0);
 // spotlight.target.updateMatrixWorld();
